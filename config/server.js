e2054a060e3796ad23ddab253282bd17c8d8f3de
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2021-03-19 20:04:58 -0600

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f1cbba24c36a586909c327bf0e513d08'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cd218141-1159-4978-9e27-4a55ac4c2c43'
+};
